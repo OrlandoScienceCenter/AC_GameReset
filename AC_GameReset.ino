017ee7eec3ac5199262423597249b1dedd2a14ec
Michael King <techguy.mking@gmail.com>
2016-11-28 08:53:30 -0500

    Added View change to reset code
    This version also incorporates full mouse moves.

--- AC_GameReset.ino
+++ AC_GameReset.ino
@@ -1,4 +1,5 @@
 #include <Keyboard.h>
+#include <Mouse.h>
 
 #define BUTTON_PIN 2
 
@@ -8,12 +9,14 @@ void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   // initialize control over the keyboard:
   Keyboard.begin();
+  Mouse.begin();
+  
 }
 
 void loop(){
   if (digitalRead(BUTTON_PIN)) {
     // do nothing until pin 2 goes low
-    delay(250);
+    delay(50);
 	}
 	else {
 	raceReset();
@@ -21,12 +24,29 @@ void loop(){
 }
 void raceReset(){
   //resets AC to start
-	  Keyboard.press(ctrlKey);
-	  Keyboard.press('o');
-  //Activates AutoHotKeyScript to click mouse on start race button
-	  Keyboard.press(ctrlKey);
-	  Keyboard.press(KEY_F12);
-  delay(100);
+   delay(10);
+	for (uint8_t i = 0; i < 16; i++){ // Arduino is stupid, so we have to loop
+		Mouse.move(-127,-127,0); // sets back to upper left corner 
+    }
+	delay(10);
+	Keyboard.press(0x80); // left ctrlkey
+	delay(50);
+	Keyboard.press('o');
+	delay(20);
   Keyboard.releaseAll();
+  delay(1000); // wait time for game to reset
+	Mouse.move(20,65,0);
+	delay(5);
+	Mouse.click();
+	delay(10);
+		for (uint8_t i = 0; i < 16; i++){ // Arduino is stupid, so we have to loop
+		Mouse.move(-127,127,0); // sets back to upper left corner 
+		}
+    Keyboard.ReleaseAll();              /// Changes View to driver
+   	  Keyboard.press(ctrlKey);
+      delay(20)
+	  Keyboard.press(KEY_F6);
+      delay(20)
+      Keyboard.releaseAll();
   delay(1000);
 }
